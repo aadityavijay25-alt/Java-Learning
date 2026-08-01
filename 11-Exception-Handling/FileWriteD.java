f371e49343fc13adc6da6ecdd0056606f06f1948
Aaditya Vijay <aaditya.vijay25@gmail.com>
2026-08-01 23:52:35 +0530

    Added File writing concepts

--- 11-Exception-Handling/FileWriteD.java
+++ 11-Exception-Handling/FileWriteD.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -6,13 +7,19 @@ public class FileWriteD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        try(FileWriter writer = new FileWriter("test.txt")){
+        String filePath = "C:\\Users\\Dell\\OneDrive\\Desktop\\test.txt";
+        String textContent = "I like Pizza!\nWould you buy for me?";
 
-            writer.write("I like Pizza!");
+
+        try(FileWriter writer = new FileWriter(filePath)){
+            writer.write(textContent);
             System.out.println("File has been written");
         }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file location");
+        }
         catch(IOException e){
-            System.out.println("Could not read file");
+            System.out.println("Could not write file");
         }
     }
 }
